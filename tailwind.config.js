bb93e73d808f0c671f8b82048cc2604e6dcba71b
gabito1451 <obiagaziegabriel@gmail.com>
2024-10-15 20:22:30 +0200

    fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,19 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        lg: "1280px",
+      },
+      height: {
+        "calc-navbar": "calc(100vh - 84px)",
+      },
+
+      // Adding custom utilities for masking
+      maskImage: {
+        "radial-blur": "radial-gradient(circle, white 60%, transparent 100%)",
+      },
+    },
   },
   plugins: [],
 };
